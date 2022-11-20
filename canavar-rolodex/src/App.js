ba3bbd83ca6e227519d3210e5d292f9a6a168f02
Akin94 <akin.tutkan@gmail.com>
2022-11-20 19:42:46 +0300

    ..

--- canavar-rolodex/src/App.js
+++ canavar-rolodex/src/App.js
@@ -1,39 +1,72 @@
-import { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class App extends Component{ //fonksiyonelliğini genişlet ve app'i class component sayesinde
-  constructor(){ // p değişse buttona basılsa da bu constructor kurucu yöntem çalıştırsın
-    super()
+import { Component } from "react";
+import CardList from "./components/card-list/card-list.component";
+import "./App.css";
+class App extends Component {
+  //fonksiyonelliğini genişlet ve app'i class component sayesinde
+  constructor() {
+    // p değişse buttona basılsa da bu constructor kurucu yöntem çalıştırsın Herşeyden önce constructor çalışır.
+    super();
     this.state = {
-      canavarlar :[
-      // {name:"Linda"},
-      // {name:"Frank"},
-      // {name:"Jackie"},
-      // {name:"Andre"}
-    ]
-    }
-  } 
-  componentDidMount(){
-    fetch(`https://jsonplaceholder.typicode.com/users`)
-   .then((res)=> res.json()) //index.js react strict mode silinirse çift konsol ortadan kalkar
-   .then((users) => this.setState(() =>{
-    return{canavarlar : users}
-   }, ()=>{
-    console.log(this.state)
-   }))
+      canavarlar: [],
+      aramaAlanı: "",
+    };
+    //console.log("constructor")
   }
-  render(){  
-  return (
-    <div className="App">{this.state.canavarlar.map((canavar)=>{
-      return (
-      <div key={canavar.name}>
-      <h1>{canavar.name}</h1>
-    </div>
-    )})}
-    </div>
-  );
-}
+  componentDidMount() { // başlangıç state değeri var sonra component did mount ile bağlandı
+    //console.log("ComponentDİd")
+    fetch(`https://jsonplaceholder.typicode.com/users`)
+      .then((res) => res.json()) //index.js react strict mode silinirse çift konsol ortadan kalkar
+      .then((users) =>
+        this.setState(
+          () => {
+            return { canavarlar: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
+  }
+  aramaDeğişikliği = (e)=>{
+    // console.log(e.target.value)
+    const aramaAlanı = e.target.value.toLocaleLowerCase()
+    //[{name:"leanne"},{name:"Namık"}]
+
+    this.setState(()=>{
+      return {aramaAlanı}
+    })
+    // ,
+    // ()=>{
+    //   console.log(
+    //     {bitişDizisi : this.state.canavarlar
+    //     })
+    // }
+    }
+  render() {    console.log("render")
+  const { canavarlar,aramaAlanı} = this.state
+  const {aramaDeğişikliği} = this
+    const filtrelenmişCanavarlar = canavarlar.filter((canavar)=>{
+     return canavar.name.toLocaleLowerCase().includes(aramaAlanı)
+    })
+    return (
+      <div className="App">
+        <input 
+        className="search-box" 
+        type="search" 
+        placeholder="search monsters"
+        onChange={aramaDeğişikliği}
+        />
+        {/* {filtrelenmişCanavarlar.map((canavar) => {
+          return (
+            <div key={canavar.name}>
+              <h1>{canavar.name}</h1>
+            </div>
+          );
+        })} */}
+        <CardList/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ canavar-rolodex/src/components/card-list/card-list.component.jsx
@@ -0,0 +1,7 @@
+import {Component} from "react"
+class CardList extends Component {
+    render() {
+        return <div>Merhaba ben kart listesi bileşeniyim</div>
+    }
+}
+export default CardList
