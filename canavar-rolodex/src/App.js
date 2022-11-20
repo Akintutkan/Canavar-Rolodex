1d4ed4a7ffefd72abb05ab9eccf1256403d019b8
Akin94 <akin.tutkan@gmail.com>
2022-11-20 19:41:41 +0300

    ..

--- canavar-rolodex/src/App.js
+++ canavar-rolodex/src/App.js
@@ -7,13 +7,22 @@ class App extends Component{ //fonksiyonelliğini genişlet ve app'i class compo
     super()
     this.state = {
       canavarlar :[
-      {name:"Linda"},
-      {name:"Frank"},
-      {name:"Jackie"},
-      {name:"Andre"}
+      // {name:"Linda"},
+      // {name:"Frank"},
+      // {name:"Jackie"},
+      // {name:"Andre"}
     ]
     }
   } 
+  componentDidMount(){
+    fetch(`https://jsonplaceholder.typicode.com/users`)
+   .then((res)=> res.json()) //index.js react strict mode silinirse çift konsol ortadan kalkar
+   .then((users) => this.setState(() =>{
+    return{canavarlar : users}
+   }, ()=>{
+    console.log(this.state)
+   }))
+  }
   render(){  
   return (
     <div className="App">{this.state.canavarlar.map((canavar)=>{
